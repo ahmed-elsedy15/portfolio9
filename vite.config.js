acd9fc4a8f16915f55d0039fac61c4f47f9e55e5
ahmed-elsedy15 <ahmed1522003aa@gmail.com>
2026-09-17 15:36:09 +0300

    sas

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/portfolio9/',   // اسم الريبو بتاعك بالظبط
   plugins: [react()],
-  base: "/portfolio9/",
-});
+})
