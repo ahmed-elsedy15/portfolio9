1b248a574830bfdf66e362a0909f7165e511b8d8
ahmed-elsedy15 <ahmed1522003aa@gmail.com>
2026-09-17 15:57:16 +0300

    gf

--- src/sections/Intro.jsx
+++ src/sections/Intro.jsx
@@ -98,7 +98,7 @@ export default function Intro() {
                 {ui.contactMe}
               </Button>
               <Button
-                href="/Ahmed_Elshahat_CV.pdf"
+                href="/Ahmed_Elshahat_CV_Frontend_Developer.pdf"
                 variant="ghost"
                 icon={Download}
                 download
